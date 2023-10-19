5cbbbf29a1eb08a35bea082809b28f58d657a420
Rayched <ny2401@naver.com>
2023-10-20 00:57:35 +0900

    프로젝트, Git Repository 연결

---
+++ src/Build_TS/testExam.ts
@@ -0,0 +1,9 @@
+//컴파일 테스트
+
+let Hello: string = "Hello";
+Hello = "Hello World";
+
+console.log(Hello);
+
+//TypeScript 컴파일 설정이 제대로 됐는 지
+//테스트하기 위한 코드
---
+++ src/Output_JS/testExam.js
@@ -0,0 +1,4 @@
+"use strict";
+let Hello = "Hello";
+Hello = "Hello World";
+console.log(Hello);
